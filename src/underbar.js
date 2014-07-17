a6c7cc80b231563081a9eb371b144639349ccb19
concreted <arichuang@gmail.com>
2014-07-17 16:23:37 -0700

    Implement some (using each)

--- src/underbar.js
+++ src/underbar.js
@@ -219,6 +219,15 @@ var _ = {};
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+      iterator = iterator || _.identity;
+
+      var result = false;
+
+      _.each(collection, function(value) {
+	  result = result || iterator(value);
+      });
+
+      return !!result;
   };
 
 
