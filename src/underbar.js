a6283cd4559a4cc1a4eaf99e06cb6053b24356c9
concreted <arichuang@gmail.com>
2014-07-18 10:21:09 -0700

    Implement memoize

--- src/underbar.js
+++ src/underbar.js
@@ -307,6 +307,15 @@ var _ = {};
   // already computed the result for the given argument and return that value
   // instead if possible.
   _.memoize = function(func) {
+      var results = {};
+
+      return function() {
+	  var arg = arguments[0];
+	  if (!(arg in results)) {
+	      results[arg] = func.call(this, arg);
+	  }
+	  return results[arg];
+      }
   };
 
   // Delays a function for the given number of milliseconds, and then calls
