134edab09eb37e550acd0a95b545cf52a8e8915e
concreted <arichuang@gmail.com>
2014-07-18 07:34:34 -0700

    Implement extend, defaults

--- src/underbar.js
+++ src/underbar.js
@@ -245,11 +245,27 @@ var _ = {};
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+      _.each(arguments, function(otherObj) {
+	  _.each(otherObj, function(value, key) {
+	      obj[key] = value;
+	  });
+      });
+      
+      return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+      _.each(arguments, function(otherObj) {
+	  _.each(otherObj, function(value, key) {
+	      if (!(key in obj)){
+		  obj[key] = value;
+	      }
+	  });
+      });
+      
+      return obj;
   };
 
 
